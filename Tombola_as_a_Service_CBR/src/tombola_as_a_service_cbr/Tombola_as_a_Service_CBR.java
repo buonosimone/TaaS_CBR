b04e785fffa2c959bed62e4d7bc4c13479d0c029
buonosimone <buono.simone@franchettisalviani.net>
2026-05-08 09:42:28 +0200

    crazy

---
+++ Tombola_as_a_Service_CBR/src/tombola_as_a_service_cbr/Tombola_as_a_Service_CBR.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tombola_as_a_service_cbr;
+
+/**
+ *
+ * @author buono.simone
+ */
+public class Tombola_as_a_Service_CBR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
